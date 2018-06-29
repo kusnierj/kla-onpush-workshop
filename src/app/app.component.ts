1e9374c1356811ebbe0dde3c2d0a909c35f889e1
alexmorriskla <a.morris@kunzleigh.com>
2018-06-28 23:14:17 -0400

    More OnPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Customer } from './model';
 import { CustomerService } from './services/customer.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
     public customerList: Customer[];
